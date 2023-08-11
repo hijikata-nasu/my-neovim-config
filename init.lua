647e5aa9f291646e3266a8a9e13862d5e1bf046d
hijikata.kaito <kaitomikkumiku+github@gmail.com>
2023-08-11 17:52:20 +0900

    カラーテーマのプラグインを導入

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 require("options")
 require("plugins")
+
+-- カラーテーマの初期化
+vim.cmd[[colorscheme tokyonight-night]] 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,9 +2,11 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-  --
+  -- (packer のクイックスタートガイド)[https://github.com/wbthomason/packer.nvim#quickstart]
   -- この下にインストールするプラグインを記載します。
   -- use 'プラグインA'
   -- use 'プラグインB'
+  -- プラグイン追加後は`:PackerInstall`でプラグインをインストールする
   --
+  use 'folke/tokyonight.nvim'
 end)
